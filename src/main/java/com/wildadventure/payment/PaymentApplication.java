236a07adb1191aff29e40a6f4789c73a3e0f6a07
Titouan Raimbault <t_raimbault@yahoo.fr>
2020-09-22 14:28:09 +0200

    #WA-53 : Add swagger 2 documentation

--- src/main/java/com/wildadventure/payment/PaymentApplication.java
+++ src/main/java/com/wildadventure/payment/PaymentApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableFeignClients("com.wildadventure")
+@EnableSwagger2
 @EnableDiscoveryClient
 public class PaymentApplication {
 
